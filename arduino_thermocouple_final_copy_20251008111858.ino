174d59dcc14c6ad086b917672629dbbdc3c82725
Nathaniel Dummer <139890511+NathanDummer@users.noreply.github.com>
2025-10-27 19:56:07 -0400

    Update thermocouple reading logic and add get_temp function
    Refactor temperature reading and median calculation.

--- arduino_thermocouple_final_copy_20251008111858.ino
+++ arduino_thermocouple_final_copy_20251008111858.ino
@@ -2,7 +2,7 @@
 #include "Adafruit_MAX31855.h"
 
 #define SAMPLE_SIZE 7
-#define MAXDO   12          // MISO (Data Out)
+#define MAXDO   8          // MISO (Data Out)
 #define MAXCS   10          // Chip Select (CS)
 #define MAXCLK  13          // Serial Clock (SCK)
 
@@ -32,31 +32,72 @@ void off() {
   while (1); 
 }
 
-double find_median(temp_one, temp_two, temp_three) {
-  if ((temp_one >= temp_two && temp_one <= temp_three) || (temp_one >= temp_three && temp_one <= temp_two)) {
-    return temp_one;
-  } else if ((temp_two >= temp_one && temp_two <= temp_three) || (temp_two >= temp_three && temp_two <= temp_one)) {
-    return temp_two;
-  } else {
-    return temp_three;
+double get_temp() {
+  int i = 0;
+  double temp;
+  unsigned long previousMillis = 0; 
+  unsigned long currentMillis = millis();
+  double readings[SAMPLE_SIZE] = {0,0,0,0,0,0,0};
+
+  while(i < SAMPLE_SIZE) {
+
+    if(currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+
+      temp = thermocouple.readCelsius();
+
+      // Serial.println(temp);
+      return temp;
+      if (!isnan(temp) && temp != null_temp) {
+        bool spot_not_found = true;
+        Serial.println("here");
+
+        for (int j = 0; j < i; j++) {
+          double temp_value;
+
+          Serial.println("here2");
+
+          if (spot_not_found && readings[j] == 0) {
+            spot_not_found = false;
+            readings[j] = temp;
+          } else if (spot_not_found && temp < readings[j]) {
+            temp_value = readings[j];
+            readings[j] = temp;
+            temp = temp_value;
+          }
+        }
+
+        readings[i] = temp;
+        i++;
+      }
+
+    }
+
   }
+
+  return readings[3]; // returning median
 }
+
+double temp_one;
+double temp_two;
+double temp_three;
 
 void loop() {
   digitalWrite(power, LOW);
+ 
   delay(1000);
-
+  double curr_temp = initial_temp;
   double delta_temp = 0.0;
-  double curr_temp, temp_one, temp_two, temp_three;
 
   Serial.print("initial_temp: ");
   Serial.println(initial_temp);
   Serial.println("Starting loop!");
-
+  
   unsigned long previousMillis = millis(); 
   unsigned long currentMillis = millis();
   
   temp_one = thermocouple.readCelsius();
+
   while (isnan(temp_one)) {
     temp_one = thermocouple.readCelsius();
   }
@@ -68,6 +109,7 @@ void loop() {
 
     if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
+
       curr_temp = thermocouple.readCelsius();
 
       if (!isnan(curr_temp)) {
@@ -79,10 +121,16 @@ void loop() {
           temp_three = curr_temp;
         }
 
-        // finding the median temp from the last three readings
-        curr_temp = find_median(temp_one, temp_two, temp_three);
+        if ((temp_one >= temp_two && temp_one <= temp_three) || (temp_one >= temp_three && temp_one <= temp_two)) {
+          curr_temp = temp_one;
+        } else if ((temp_two >= temp_one && temp_two <= temp_three) || (temp_two >= temp_three && temp_two <= temp_one)) {
+          curr_temp = temp_two;
+        } else {
+          curr_temp = temp_three;
+        }
+
         delta_temp = curr_temp - initial_temp;
-        
+
         Serial.print("curr_temp: ");
         Serial.print(curr_temp);
         Serial.print(", delta_temp: ");
@@ -99,5 +147,6 @@ void loop() {
 
     currentMillis = millis();
   }
+
   off();
 }
